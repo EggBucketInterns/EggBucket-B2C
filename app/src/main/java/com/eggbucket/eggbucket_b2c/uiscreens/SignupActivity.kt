35791de5fa13eaaf5fad6ece225f3445eada11ae
2535ShekharMourya <113845874+2535ShekharMourya@users.noreply.github.com>
2024-09-27 23:47:32 +0530

    UI completed

--- app/src/main/java/com/eggbucket/eggbucket_b2c/uiscreens/SignupActivity.kt
+++ app/src/main/java/com/eggbucket/eggbucket_b2c/uiscreens/SignupActivity.kt
@@ -1,23 +1,17 @@
-package com.eggbucket.eggbucket_b2c
+package com.eggbucket.eggbucket_b2c.uiscreens
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.eggbucket.eggbucket_b2c.uiscreens.LoginActivity
+import com.eggbucket.eggbucket_b2c.R
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val gotoNext=findViewById<TextView>(R.id.tex1)
-        gotoNext.setOnClickListener {
-            startActivity(Intent(this@MainActivity,LoginActivity::class.java))
-        }
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
