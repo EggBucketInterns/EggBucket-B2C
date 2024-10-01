db53c3cdfc8a1275d8de9bd475ae91e4e25c1dd3
Shubham-344 <shubhamanand030404@gmail.com>
2024-10-01 19:51:10 +0530

    final

---
+++ app/src/main/java/com/eggbucket/eggbucket_b2c/BottomNavigation/ui/basket/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.eggbucket.eggbucket_b2c.BottomNavigation.ui.basket
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
