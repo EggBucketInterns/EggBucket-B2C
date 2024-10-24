4bf9067a75a70f052fb6d8de7225fe4550dac15d
Shree sudhanva <shreesudhanva2004@gmail.com>
2024-10-24 22:34:34 +0530

    updated address

--- app/src/main/java/com/eggbucket/eggbucket_b2c/AddAddressActivity.kt
+++ app/src/main/java/com/eggbucket/eggbucket_b2c/AddAddressActivity.kt
@@ -1,27 +1,39 @@
 package com.eggbucket.eggbucket_b2c
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
-class AddAddressActivity : AppCompatActivity() {
+class AddAddressActivity : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_address)
-
-        // Get the view with ID 'main'
-        val mainView = findViewById<View>(R.id.main)
-
-        // Check if the view is not null and apply the window insets listener
-        mainView?.post {
-            ViewCompat.setOnApplyWindowInsetsListener(mainView) { view, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            }
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_add_address, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//         Set up any logic or listeners here
+//        btn_submit.setOnClickListener {
+//            // Handle the button click here
+//            val name = et_name.text.toString()
+//            val address = et_address1.text.toString()
+//            val city = et_city.text.toString()
+//            val state = et_state.text.toString()
+//            val postalCode = et_postal_code.text.toString()
+//            val flatNo = flat_no.text.toString()
+//            val landmark = landmark.text.toString()
+//
+//             Process or save the address details
+//        }
+   }
 }
